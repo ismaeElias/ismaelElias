ef2e62a62c67fb018a9fbc00d6b2c439f9f95aab
ismael.elias <ismael.elias@betha.com.br>
2022-11-26 18:47:29 -0300

    feat: Header web

--- src/App.jsx
+++ src/App.jsx
@@ -1,45 +1,15 @@
-import { Box, Center, Circle, Text } from "@chakra-ui/react";
-import { BsGithub, BsLinkedin } from "react-icons/bs";
-import CodingAnimation from "./components/codingAnimation";
+import {Box, Center} from "@chakra-ui/react";
+import Header from "./components/Header/index.jsx";
+
 function App() {
-  return (
-    <Box h="100vh" backgroundRepeat="no-repeat" backgroundSize="cover" backgro>
-      <Center>
-        <Box
-          display={{ sm: "flex" }}
-          flexDirection={{ sm: "column", md: "row", base: "row" }}
-        >
-          <Center flexDir="column">
-            <Text fontWeight="bold">Site em construção</Text>
-            <Text>Em breve mais novidades sobre meu portifolio</Text>
-          </Center>
-          <CodingAnimation />
+    return (
+        <Box h="100vh">
+            <Header/>
+            <Center>
+                
+            </Center>
         </Box>
-      </Center>
-      <Box position="absolute" bottom="0" w="full" mb={1}>
-        <Center gap={5}>
-          <p>
-            Desenvolvido por
-            <strong> Ismael Elias</strong> | &#9889;
-          </p>
-          <a href="https://github.com/ismaeElias" target="blank">
-            <Circle size="40px" bg="gray.300" boxShadow="md">
-              <BsGithub></BsGithub>
-            </Circle>
-          </a>
-          <a
-            href="https://www.linkedin.com/in/ismael-eliass/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Circle size="40px" bg="gray.300" boxShadow="md">
-              <BsLinkedin></BsLinkedin>
-            </Circle>
-          </a>
-        </Center>
-      </Box>
-    </Box>
-  );
+    );
 }
 
 export default App;
---
+++ src/components/Header/index.jsx
@@ -0,0 +1,47 @@
+import React from 'react';
+import {Button, Flex, Text} from "@chakra-ui/react";
+import {TbChartLine, TbHome2, TbNotes, TbSend} from "react-icons/all.js";
+
+function Header() {
+    return (
+        <Flex
+            h="60px"
+            w="100vw"
+            position={"fixed"}
+            zIndex={"2"}
+            bgColor="white"
+            alignItems={"center"}
+            justifyContent={"space-around"}
+            boxShadow='md'
+        >
+            <Flex
+                direction="column"
+                alignItems="center"
+                cursor="default"
+            >
+                <Text fontSize={"lg"} fontWeight="bold" color="orange.400">ISMAEL ELIAS</Text>
+                <Text fontSize={"sm"}>DESENVOLVEDOR FULL-STACK</Text>
+            </Flex>
+            <Flex gap="4"
+                  alignItems="center">
+                <Button colorScheme='orange' variant='ghost' leftIcon={<TbHome2/>
+                }>
+                    INICIO
+                </Button>
+                <Button colorScheme='orange' variant='ghost' leftIcon={<TbNotes/>
+                }>
+                    SOBRE
+                </Button>
+                <Button colorScheme='orange' variant='ghost' leftIcon={<TbChartLine/>
+                }>
+                    EXPERIENCIA
+                </Button>
+
+                <Button colorScheme="orange" rightIcon={<TbSend/>}>CONTATO</Button>
+            </Flex>
+        </Flex>
+
+    );
+}
+
+export default Header;
